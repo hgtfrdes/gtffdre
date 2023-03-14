ec7fe1edb4dbe80abd4b33462d14dd5e1e6ada31
hgtfrdes <127807440+hgtfrdes@users.noreply.github.com>
2023-03-14 05:17:18 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=40970ba1-bed3-4737-bdbf-a3a27e98e9e3
+export UUID=7cf51b83-7755-4cfb-a605-99c8aa03f39f
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=https://hcovers.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
