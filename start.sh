960daa1dd9a49583c4a964ed139bd66061bf3aec
hgtfrdes <127807440+hgtfrdes@users.noreply.github.com>
2023-03-14 04:02:42 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=40970ba1-bed3-4737-bdbf-a3a27e98e9e3
 
 #请修改为自己设置的伪装站，不要带https://
 export ProxySite=www.holehike.com
